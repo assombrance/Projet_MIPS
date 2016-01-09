43f9dedbb9e35c5def55133cc7bd639fbfb3a043
AlexandreMIPS <alexandre.eid@etu.esisar.grenoble-inp.fr>
2016-01-09 12:08:49 +0100

    binaryToHexa_v2.0 Implémentation du message d'erreur lors de nombres négatifs

--- decimalToBinary.c
+++ decimalToBinary.c
@@ -18,6 +18,12 @@ char* decimalToBinary(int decimal) {
 
 	binary = malloc(sizeof(*binary) + 32);
 
+	if (decimal < 0) {
+		fprintf(stderr," \n ************decimalToBinary_v2.0************* \n Sorry, this version does not work with negativ numbers \n Our developpers are on it. \n Thanks for your patience. \n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*Ne fonctionne pas avec des nombres négatifs*/
 	for (i = 0; i <= SIZE_MAX; i++) {
 
 		res = decimal - (1 << power);
