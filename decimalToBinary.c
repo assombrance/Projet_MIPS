7d0275364f57ad10eb935ede7c8dd2082c1bfa2f
AlexandreMIPS <alexandre.eid@etu.esisar.grenoble-inp.fr>
2016-01-09 10:16:32 +0100

    decimalToBinaryv1.0 L'algorithme ne fonctionne que pour les puissances  de 2. Problème dans le tableau pour regrouper les puissances.

--- decimalToBinary.c
+++ decimalToBinary.c
@@ -11,31 +11,35 @@
 char* decimalToBinary(int decimal) {
 
 	int res = 1; /*juste pour res soit différent de 0*/
-	unsigned int powerMax = 0;
-	unsigned int i;
+	unsigned int maximumPower = 0;
+	unsigned int i = 0;
 	char* binary = NULL;
-	char* power = NULL;
+	int power[MAX];
+	int TEMP;
 
 	binary = malloc(sizeof(*binary) * (MAX + 1));
-	power = malloc(sizeof(*power) * (MAX + 1));
+	/* power = malloc(sizeof(*power) * (MAX + 1));*/
+
 
 	/*Définir la puissance de 2 à calculer */
 
 	while (decimal > 0) {
 
-		do {
-			res = decimal - (2 << powerMax);
-			powerMax++;
-		} while (res > 0);
-
-		decimal -= (2 << powerMax);
+		while (res > 0) {
+			res = decimal - (1 << maximumPower);
+			maximumPower++;
+		}
+		maximumPower = maximumPower - 2; /*La boucle while fait 2 tours de trop... penser à revoir la condition d'arrêt*/
+		decimal -= (1 << maximumPower);
 		
-		binary[powerMax] = '1';
+		/*tableau regroupant les puissances de deux successives*/
+		power[i] = maximumPower;
 
+		i++;
 		res = 1;
-		powerMax = 0;
+		maximumPower = 0;
 	}
-	
+	power[i] = '\0';
 
 
 
