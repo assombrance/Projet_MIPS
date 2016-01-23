64bf29f998ff57229d7c6bd048848b8bd31f1616
assombrance <assombrance.anderson@gmail.com>
2016-01-23 16:16:20 +0100

    correction de sw

--- Emulateur/sw.c
+++ Emulateur/sw.c
@@ -2,9 +2,8 @@
 
 void swEmul(char* instruction,char* memoire,int32_t* registres) {
 
-	int32_t rs, rt, rd;
-	int i=0,j=0, rt_instruction, rs_instruction, rd_instruction;
-	char rs_w[3], rt_w[3], rd_w[3];
+	int i=0,j=0, rt_instruction, off_instruction;
+	char rt_w[3], off_w[7];
 
 	while ((instruction[i] == ' ') || (instruction[i] == '\t')) { //passage au sw
 		i++;
@@ -12,44 +11,29 @@ void swEmul(char* instruction,char* memoire,int32_t* registres) {
 	while ((instruction[i] != ' ' ) && (instruction[i] != '\t')) { //passage de l'sw
 		i++;
 	}
-	while ((instruction[i] == ' ') || (instruction[i] == '\t')) { //passage à la première opérande (rd)
-		i++;
-	}
-	rd_w[0] = instruction[i];
-	i++;j++;
-	if ((instruction[i] != ' ') && (instruction[i] != '\t') && (instruction[i] != ',')) {
-		rd_w[1] = instruction[i];
-		i++;j++;
-	}
-	rd_w[j] = '\0';j = 0;
-	while ((instruction[i] == ' ') || (instruction[i] == '\t') || (instruction[i] == ',')) { //passage à la deuxième opérande (rs)
-		i++;
-	}
-	rs_w[0] = instruction[i];
-	i++;j++;
-	if ((instruction[i] != ' ') && (instruction[i] != '\t') && (instruction[i] != ',')) {
-		rs_w[1] = instruction[i];
-		i++;j++;
-	}
-	rs_w[j] = '\0';j = 0;
-	while ((instruction[i] == ' ') || (instruction[i] == '\t') || (instruction[i] == ',')) { //passage à la troisième opérande (rt)
+	while ((instruction[i] == ' ') || (instruction[i] == '\t')) { //passage à la première opérande (rt)
 		i++;
 	}
 	rt_w[0] = instruction[i];
 	i++;j++;
-	if ((instruction[i] != ' ') && (instruction[i] != '\t') && (instruction[i] != '\0') && (instruction[i] != '#')) {
+	if ((instruction[i] != ' ') && (instruction[i] != '\t') && (instruction[i] != ',')) {
 		rt_w[1] = instruction[i];
 		i++;j++;
 	}
-	rt_w[j] = '\0';
+	rt_w[j] = '\0';j = 0;
+	while ((instruction[i] == ' ') || (instruction[i] == '\t') || (instruction[i] == ',')) { //passage à la deuxième opérande (off)
+		i++;
+	}
+	off_w[0] = instruction[i];
+	i++;j++;
+	while ((instruction[i] != ' ') && (instruction[i] != '\t') && (instruction[i] != ',')) {
+		off_w[1] = instruction[i];
+		i++;j++;
+	}
+	off_w[j] = '\0';
 	
 	rt_instruction = atoi(rt_w);
-	rd_instruction = atoi(rd_w);
-	rs_instruction = atoi(rs_w);
+	off_instruction = atoi(off_w);
 
-	rt = registres[rt_instruction];
-	rs = registres[rs_instruction];
-	rd = registres[rd_instruction];
-
-	memoire[rd]=memoire[rs]-memoire[rt];
+	memoire[off]=registres[rt_instruction];
 }
