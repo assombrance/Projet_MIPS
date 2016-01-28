7f4883a8f9890a9e45f024a8d27645561247b496
assombrance <assombrance.anderson@gmail.com>
2016-01-28 22:04:38 +0100

    fin !

--- Emulateur/convMnemoniqueEmul.c
+++ Emulateur/convMnemoniqueEmul.c
@@ -16,76 +16,76 @@ void convMnemoniqueEmul(char* ligne, char* memoire, int32_t* registres) {
 	}
 	operation[j] = '\0';
 
-	if (strcmp(operation, "add")) {
+	if (strcmp(operation, "add") == 0 ) {
 		addEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "addi")) {
+	else if (strcmp(operation, "addi") == 0 ) {
 		addiEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "sub")) {
+	else if (strcmp(operation, "sub") == 0 ) {
 		subEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "mult")) {
+	else if (strcmp(operation, "mult") == 0 ) {
 		multEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "div")) {
+	else if (strcmp(operation, "div") == 0 ) {
 		divEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "and")) {
+	else if (strcmp(operation, "and") == 0 ) {
 		andEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "or")) {
+	else if (strcmp(operation, "or") == 0 ) {
 		orEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "xor")) {
+	else if (strcmp(operation, "xor") == 0 ) {
 		xorEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "rotr")) {
+	else if (strcmp(operation, "rotr") == 0 ) {
 		rotrEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "sll")) {
+	else if (strcmp(operation, "sll") == 0 ) {
 		sllEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "srl")) {
+	else if (strcmp(operation, "srl") == 0 ) {
 		srlEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "slt")) {
+	else if (strcmp(operation, "slt") == 0 ) {
 		sltEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "lw")) {
+	else if (strcmp(operation, "lw") == 0 ) {
 		lwEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "sw")) {
+	else if (strcmp(operation, "sw") == 0 ) {
 		swEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "lui")) {
+	else if (strcmp(operation, "lui") == 0 ) {
 		luiEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "mfhi")) {
+	else if (strcmp(operation, "mfhi") == 0 ) {
 		mfhiEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "mflo")) {
+	else if (strcmp(operation, "mflo") == 0 ) {
 		mfloEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "beq")) {
+	else if (strcmp(operation, "beq") == 0 ) {
 		beqEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "bne")) {
+	else if (strcmp(operation, "bne") == 0 ) {
 		bneEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "bgtz")) {
+	else if (strcmp(operation, "bgtz") == 0 ) {
 		bgtzEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "blez")) {
+	else if (strcmp(operation, "blez") == 0 ) {
 		blezEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "j")) {
+	else if (strcmp(operation, "j") == 0 ) {
 		jEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "jal")) {
+	else if (strcmp(operation, "jal") == 0 ) {
 		jalEmul(ligne, memoire, registres);
 	}
-	else if (strcmp(operation, "jr")) {
+	else if (strcmp(operation, "jr") == 0 ) {
 		jrEmul(ligne, memoire, registres);
 	}
 	else {
@@ -93,13 +93,15 @@ void convMnemoniqueEmul(char* ligne, char* memoire, int32_t* registres) {
 	}
 
 	printf("%s\n",ligne);
-	addiEmul(ligne, memoire, registres);
 
-	printf("____________________________________________________________________________________\n");
-	printf("registre n° :\t1\t 2\t 3\t 4\t 5\t HI\t LO\n");
-	printf("contenu :\t%d \t %d \t %d \t %d \t %d \t %d \t %d\n",registres[1],registres[2],registres[3],registres[4],registres[5],registres[34], registres[33]);
-	printf("____________________________________________________________________________________\n");
-	printf("mémoire :\t1\t 2\t 3\t 4\t 5\n");
-	printf("\t\t%d \t %d \t %d \t %d \t %d\n", memoire[1],memoire[2],memoire[3],memoire[4],memoire[5]);
-	printf("____________________________________________________________________________________\n\n");
+	if (pasAPas);
+		printf("____________________________________________________________________________________\n");
+		printf("registre n° :\t1\t 2\t 3\t 4\t 5\t HI\t LO\t PC\n");
+		printf("contenu :\t%d \t %d \t %d \t %d \t %d \t %d \t %d\t %d\n",registres[1],registres[2],registres[3],registres[4],registres[5], registres[34], registres[33],registres[32]);
+		printf("____________________________________________________________________________________\n");
+		printf("mémoire :\t1\t 2\t 3\t 4\t 5\n");
+		printf("\t\t%d \t %d \t %d \t %d \t %d\n", memoire[1],memoire[2],memoire[3],memoire[4],memoire[5]);
+		printf("____________________________________________________________________________________\n\n");
+		getchar();
+	}
 }
--- Emulateur/main.c
+++ Emulateur/main.c
@@ -3,16 +3,21 @@
 
 int main(int argc, char* argv[]) {
 
-	char* memoire = NULL;
-	int32_t* registres = NULL; // registre spéciaux : 32->PC le Programm counter ; 33->LO (les bits de poids faible); 34->HI (les bits de poids fort)
+	char memoire[1024];
+	int32_t registres[35]; // registre spéciaux : 32->PC le Programm counter ; 33->LO (les bits de poids faible); 34->HI (les bits de poids fort)
 	char* BUFFER = NULL;
 	int tailleSource = 0, position = 0; //en lignes
 	int character, i;
 	FILE* source = NULL;
 
-	registres = malloc(sizeof(*registres) * 35);
-	memoire = malloc(sizeof(*memoire) * 1024);
+	//registres = malloc(sizeof(*registres) * 35);
+	//memoire = malloc(sizeof(*memoire) * 1024);
 	BUFFER = malloc(sizeof(*BUFFER) * 32);
+	
+	for (i=0;i<35;i++) registres[i]=0;
+	for (i=0;i<1024;i++) memoire[i]=0;
+
+
 	registres[32]=0;
 
 	registres[1]=1;
@@ -22,8 +27,8 @@ int main(int argc, char* argv[]) {
 	memoire[1]=82;
 
 	printf("____________________________________________________________________________________\n");
-	printf("registre n° :\t1\t 2\t 3\t 4\t 5\t HI\t LO\n");
-	printf("contenu :\t%d \t %d \t %d \t %d \t %d \t %d \t %d\n",registres[1],registres[2],registres[3],registres[4],registres[5],registres[34], registres[33]);
+	printf("registre n° :\t1\t 2\t 3\t 4\t 5\t HI\t LO\t PC\n");
+	printf("contenu :\t%d \t %d \t %d \t %d \t %d \t %d \t %d\t %d\n",registres[1],registres[2],registres[3],registres[4],registres[5], registres[34], registres[33],registres[32]);
 	printf("____________________________________________________________________________________\n");
 	printf("mémoire :\t1\t 2\t 3\t 4\t 5\n");
 	printf("\t\t%d \t %d \t %d \t %d \t %d\n", memoire[1],memoire[2],memoire[3],memoire[4],memoire[5]);
@@ -31,15 +36,21 @@ int main(int argc, char* argv[]) {
 
 	miseEnForme("source.txt", "sourceNormalisee.txt");
 	tailleSource = taille("sourceNormalisee.txt");
-	
-	do {
-		source = fopen("sourceNormalisee", "r");
 
-		do { // on va à la bonne position
+	do {
+		source = fopen("sourceNormalisee.txt", "r");
+
+		if ( registres[32] > tailleSource*4 ) {
+			fprintf(stderr, "PC en dehors de la source\n");
+			exit(EXIT_FAILURE);
+		}
+
+		while (position != registres[32]/4) { // on va à la bonne position ... à tuiliser ? :  (character != EOF)
 			character = fgetc(source);
 			if ((char)character == '\n') position++;
-		} while ((position != registres[32]/4) && (character != EOF));
-		
+		} 
+
+		i = 0;
 		do {//on recopie la ligne dans BUFFER
 			character = fgetc(source);
 			BUFFER[i] = character;
@@ -50,14 +61,12 @@ int main(int argc, char* argv[]) {
 		i = 0;
 		registres[32]+=4; //une instruction fait 4 octets
 
-		if ( registres[32] > tailleSource*4 ) {
-			fprintf(stderr, "PC en dehors de la source\n");
-			exit(EXIT_FAILURE);
-		}
 
 		fclose(source);
 		position = 0;
-	} while (character != EOF);
+	} while ( registres[32] > tailleSource*4 );
+
+	free(BUFFER);
 
 	return 0;
 }
